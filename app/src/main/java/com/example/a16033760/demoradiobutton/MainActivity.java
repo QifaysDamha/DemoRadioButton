3871599a6c7e9823a3f9f8dfffc71388f74f3cdd
16033760 <16033760@myrp.edu.sg>
2018-04-16 21:48:16 +0800

    updating

--- app/src/main/java/com/example/a16033760/demoradiobutton/MainActivity.java
+++ app/src/main/java/com/example/a16033760/demoradiobutton/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.a16033760.demoradiobutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnOK;
+    RadioGroup rgBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOK = (Button)findViewById(R.id.button1);
+
+
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rgBtn = (RadioGroup)findViewById(R.id.radioGroup1);
+
+                int selectedButton = rgBtn.getCheckedRadioButtonId();
+
+                RadioButton rBtn = (RadioButton)findViewById(selectedButton);
+
+                Toast.makeText(getBaseContext(), rBtn.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
